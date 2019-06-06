e7cad4e7b0e2953f7973ec359cab357d4eae4797
Marek Tutaj <mtutaj@mcw.edu>
2019-06-06 09:32:55 -0500

    in sql queries replaced WM_CONCAT with LISTAGG

--- scripts/get_multiple_VT_annotations.sql
+++ scripts/get_multiple_VT_annotations.sql
@@ -1,6 +1,8 @@
-select q.rgd_id, qtl_symbol, count(*) as count, to_char(wm_concat(ot.term)) as Terms, to_char(wm_concat(fa.term_acc)) as Ontology_IDs 
-from FULL_ANNOT fa, QTLS q, ONT_TERMS ot
-where
+SELECT q.rgd_id, qtl_symbol, COUNT(*) as count,
+  LISTAGG(ot.term) WITHIN GROUP (ORDER BY ot.term) as Terms,
+  LISTAGG(fa.term_acc) WITHIN GROUP (ORDER BY fa.term_acc) as Ontology_IDs
+FROM full_annot fa, qtls q, ont_terms ot
+WHERE
 fa.ANNOTATED_OBJECT_RGD_ID = q.RGD_ID
 and fa.TERM_ACC like 'VT:%' 
 and fa.TERM_ACC = ot.TERM_ACC
