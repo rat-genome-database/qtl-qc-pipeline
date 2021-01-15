e7cd55057d0e4764800b81f278503a47cba284ab
tutajm <mtutaj@mcw.edu>
2021-01-15 10:58:49 -0600

    tuned up logging and sql queries

--- src/main/java/edu/mcw/rgd/pipelines/Dao.java
+++ src/main/java/edu/mcw/rgd/pipelines/Dao.java
@@ -24,8 +24,8 @@ public class Dao {
 
         String sql =
                 "SELECT q.rgd_id || CHR(9) || qtl_symbol || CHR(9) || COUNT(*) || CHR(9) ||"+
-                        "  LISTAGG(ot.term) WITHIN GROUP (ORDER BY ot.term) || CHR(9) ||"+
-                        "  LISTAGG(fa.term_acc) WITHIN GROUP (ORDER BY fa.term_acc) "+
+                        "  LISTAGG(ot.term, ',') WITHIN GROUP (ORDER BY ot.term) || CHR(9) ||"+
+                        "  LISTAGG(fa.term_acc, ',') WITHIN GROUP (ORDER BY fa.term_acc) "+
                         "FROM full_annot fa, qtls q, ont_terms ot "+
                         "WHERE fa.ANNOTATED_OBJECT_RGD_ID = q.RGD_ID"+
                         "  AND fa.TERM_ACC like 'CMO:%'"+
@@ -46,8 +46,8 @@ public class Dao {
 
         String sql =
         "SELECT q.rgd_id || CHR(9) || qtl_symbol || CHR(9) || COUNT(*) || CHR(9) ||"+
-        "  LISTAGG(ot.term) WITHIN GROUP (ORDER BY ot.term) || CHR(9) ||"+
-        "  LISTAGG(fa.term_acc) WITHIN GROUP (ORDER BY fa.term_acc) "+
+        "  LISTAGG(ot.term, ',') WITHIN GROUP (ORDER BY ot.term) || CHR(9) ||"+
+        "  LISTAGG(fa.term_acc, ',') WITHIN GROUP (ORDER BY fa.term_acc) "+
         "FROM full_annot fa, qtls q, ont_terms ot "+
         "WHERE fa.ANNOTATED_OBJECT_RGD_ID = q.RGD_ID"+
         "  AND fa.TERM_ACC like 'VT:%'"+
