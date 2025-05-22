c69a943c9390ae8558a1915a83a084262dc7a4fa
tutajm <mtutaj@mcw.edu>
2025-05-22 16:13:34 -0500

    fixed script -- new mail program

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -7,7 +7,7 @@ APPDIR=/home/rgddata/pipelines/$APPNAME
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 EMAIL_LIST=mtutaj@mcw.edu
 if [ "$SERVER" = "REED" ]; then
-  EMAIL_LIST=mtutaj@mcw.edu,slaulede@mcw.edu
+  EMAIL_LIST=mtutaj@mcw.edu slaulede@mcw.edu
 fi
 
 cd $APPDIR
